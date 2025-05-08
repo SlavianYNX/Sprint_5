529e6f714e7f93162ad11f6100aaf0c77bbca8f5
Slava Eremin <slavik.eremin@yandex.ru>
2025-05-08 13:43:10 +0300

    Add data

---
+++ tests/data.py
@@ -0,0 +1,12 @@
+from random import randint
+
+class Person:
+    name = 'Вячеслав'
+    email = f'slavik_eremin_22_777@yandex.ru'
+    password = f'12345678'
+    password_bad = f'12345'
+
+class RandomData:
+    name = 'Вячеслав'
+    email = f'slavik_eremin_22_{randint(0, 999)}@yandex.ru'
+    password = f'123{randint(10000, 99999)}qwe'
