f678211244f48c2c2058976c347b409aa166c96c
Slava Eremin <slavik.eremin@yandex.ru>
2025-05-08 13:52:01 +0300

    Add class PageUrls

---
+++ tests/urls.py
@@ -0,0 +1,6 @@
+class PageUrls:
+    reg_page = 'https://stellarburgers.nomoreparties.site/register'
+    auth_page = 'https://stellarburgers.nomoreparties.site/login'
+    main_page = 'https://stellarburgers.nomoreparties.site/'
+    forgot_page = 'https://stellarburgers.nomoreparties.site/forgot-password'
+    profile_page = 'https://stellarburgers.nomoreparties.site/account/profile'
